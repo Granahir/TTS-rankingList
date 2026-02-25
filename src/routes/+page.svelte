9f76aa44ed4c58307592b0e28c751ec47677f501
Granahir <georges.corbineau@gmail.com>
2026-02-25 13:40:53 +0000

    fix typos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,20 +13,20 @@
     </p>
     <p>
       Lists will be published after each national event. Although local results may be inputted in
-      between times, national tournaments will take ratings from the last national event allowing
-      national events to take priority.
+      between times, national lists will take ratings from the last national event allowing national
+      events to take priority.
     </p>
     <p>
       Players that have a Ratings Accuracy over 150 will not get a ranking position, players can
-      play more frequently to keep their rating accurate
+      play more frequently to keep their rating accurate.
     </p>
     <p>
-      <b>Full list</b> has all affiliated players that have played a competitive match<br /><b
+      <b>Full list</b> has all affiliated players that have played a competitive match.<br /><b
         >Rating List</b>
       is a subset of the full list in which a player has played one eligible national event in the past
-      12 months <br /><b>Ranking List</b> is a subset of the rating list of players that are eligible
+      12 months. <br /><b>Ranking List</b> is a subset of the rating list of players that are eligible
       to play for Scotland and play in the National Championships (Self Selected by players - this needs
-      to be checked)
+      to be checked).
     </p>
     <p>
       <br />
